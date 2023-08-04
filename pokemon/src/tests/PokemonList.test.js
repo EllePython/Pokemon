0c3763f68a35827ace8f4d511c3e60211091f535
Eliana_Lugo <Ellelugo91@gmail.com>
2023-08-03 23:53:32 -0400

    added 2 test for functionality

---
+++ pokemon/src/tests/PokemonList.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import PokemonList from '../components/PokemonList';
+
+
+test('fetches Pokemon data from API and displays them', async () => {
+    const mockPokemonList = {
+        results: [
+            { name: 'bulbasaur' },
+            { name: 'charmander' },
+            { name: 'squirtle' },
+        ],
+    };
+
+    global.fetch = jest.fn(() =>
+        Promise.resolve({
+            json: () => Promise.resolve(mockPokemonList),
+        })
+    );
+
+    render(<PokemonList />);
+
+    const bulbasaurElement = await screen.findByText('bulbasaur');
+    const charmanderElement = await screen.findByText('charmander');
+    const squirtleElement = await screen.findByText('squirtle');
+
+    expect(bulbasaurElement).toBeInTheDocument();
+    expect(charmanderElement).toBeInTheDocument();
+    expect(squirtleElement).toBeInTheDocument();
+});
+
+test('error message on failed fetch', async () => {
+    global.fetch = jest.fn(() => Promise.reject(new Error('Failed to fetch data')));
+    render(<PokemonList />);
+
+    const errorMessage = await screen.findByText('Error fetching Pokemon');
+
+    expect(errorMessage).toBeInTheDocument();
+})
